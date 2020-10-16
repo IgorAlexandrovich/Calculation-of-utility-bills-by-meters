c39984745d01c42d67dfbe83f80c648f6bdc5ce5
IgorAlexandrovich <69768948+IgorAlexandrovich@users.noreply.github.com>
2020-10-16 14:19:14 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -8,9 +8,6 @@ $payment = $_POST['payment'];
 $mail_vvod = $_POST['mail'];
 $config = require "config.php";
 
-
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
@@ -21,14 +18,6 @@ $mail->Port = 465; // TCP port to connect to / этот порт может от
 
 $mail->setFrom($config['mail']); // от кого будет уходить письмо?
 $mail->addAddress($mail_vvod);     // Кому будет уходить письмо 
-//$mail->addAddress('sn-@list.ru');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
-
 $mail->Subject = 'Сумма за комуналку';
 $mail->Body    = '' .$payment. ' рублей должны квартиранты за комуналку ';
 $mail->AltBody = '';
