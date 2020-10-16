646bda883c6e93a7d77b9a6e90b4dc61d1093720
IgorAlexandrovich <69768948+IgorAlexandrovich@users.noreply.github.com>
2020-10-16 16:46:14 +0300

    выбираем из бд предыдущие показания

---
+++ bd.php
@@ -0,0 +1,12 @@
+<?php
+$config = require "config.php";
+$sql = new mysqli('localhost',$config['login_bd'],$config['password_bd'],$config['login_bd']);
+
+$queryString ="SELECT `date`, `electricity_day`, `electricity_night`, `hot_water`, `cold_water` FROM `indications`" ;
+$responce = $sql->query($queryString);
+$users = $responce->fetch_all(); 
+$users = json_encode($users);
+echo $users
+
+
+?>
