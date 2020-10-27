fe5dcc94c6696c9c9f13a2c9c271977bfe849cac
IgorAlexandrovich <69768948+IgorAlexandrovich@users.noreply.github.com>
2020-10-27 16:55:14 +0300

    Update bd.php

--- bd.php
+++ bd.php
@@ -2,7 +2,7 @@
 $config = require "config.php";
 $sql = new mysqli('localhost',$config['login_bd'],$config['password_bd'],$config['login_bd']);
 
-$queryString ="SELECT `date`, `electricity_day`, `electricity_night`, `hot_water`, `cold_water` FROM `indications`" ;
+$queryString ="SELECT `date`, `electricity_day`, `electricity_night`, `cold_water`, `hot_water` FROM `indications`" ;
 $responce = $sql->query($queryString);
 $users = $responce->fetch_all(); 
 $users = json_encode($users);
