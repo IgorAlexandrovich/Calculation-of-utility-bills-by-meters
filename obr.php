b3b378dec63e8ee142d292ef9451fb625dd3eca8
IgorAlexandrovich <69768948+IgorAlexandrovich@users.noreply.github.com>
2020-10-29 19:34:27 +0300

    подключение к бд добавил

--- obr.php
+++ obr.php
@@ -5,5 +5,7 @@ $login = $_POST['login'];
 $password = $_POST['password'];
 var_dump ($_POST);
 
+$config = require "config.php";
+$sql = new mysqli('localhost',$config['login_bd'],$config['password_bd'],$config['login_bd']);
 
 ?>
