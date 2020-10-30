c59f640a8dfdecf9177cbc971ff2f7b6e3102a01
IgorAlexandrovich <69768948+IgorAlexandrovich@users.noreply.github.com>
2020-10-30 16:28:05 +0300

    добавил проверки

--- node_test.js
+++ node_test.js
@@ -1,17 +1,23 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql');
 const config = require('./config'); // подключаем модуль с данными для входа
 
-async function main() {
-    const conn = await mysql.createConnection(config);// подключаемся к базе
-    const rows = await conn.execute('SELECT * FROM `user_test` WHERE 1');//вытаскиваем нужную инфу из базы
 
+const conn = mysql.createConnection(config);
 
-    return rows
-}
+conn.connect(function (err) {
+    if (err) {
+        return console.error("Ошибка: " + err.message);
+    }
+    else {
+        console.log("Подключение к серверу MySQL успешно установлено ");
+    }
+});
 
-async function f() {
-    let a = await main()
-    console.log(a[0]);
+let query="SELECT * FROM user_test";
 
-}
-f() 
+conn.query(query, (err, result, field) =>{
+    console.log(err);
+    console.log(result);
+     // console.log(field);
+});
+ 
