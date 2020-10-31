8747ebeaa56d6ed2391fb8a94078038c40002f95
IgorAlexandrovich <69768948+IgorAlexandrovich@users.noreply.github.com>
2020-10-31 16:20:07 +0300

    получаем данные со страницы

--- node_test.js
+++ node_test.js
@@ -1,5 +1,25 @@
 const mysql = require('mysql');
 const config = require('./config'); // подключаем модуль с данными для входа
+const http = require('http');
+const { parse } = require('querystring');// для разбора строки пост запроса
+//POST
+http.createServer(
+    (request, response) => {
+  let body = '';
+            request.on('data', chunk => {
+                body += chunk.toString();
+            });
+            request.on('end', () => {
+                console.log(body);
+                 let params = parse(body);
+                 console.log(params);
+                 console.log(params.hi);
+                response.end('ok');
+            });
+    }
+).listen(3000);
+
+     
 
 
 const conn = mysql.createConnection(config);
@@ -13,11 +33,12 @@ conn.connect(function (err) {
     }
 });
 
-let query="SELECT * FROM user_test";
+let query= "SELECT * FROM user_test WHERE BINARY login='igor' AND BINARY password='bar'";
 
 conn.query(query, (err, result, field) =>{
-    console.log(err);
+    //console.log(err);
     console.log(result);
      // console.log(field);
 });
  
+
