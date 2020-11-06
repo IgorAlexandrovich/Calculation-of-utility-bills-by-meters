e63adb4358fdd742ab3f922249626746da082f02
IgorAlexandrovich <69768948+IgorAlexandrovich@users.noreply.github.com>
2020-11-06 17:49:06 +0300

    создал функцию удаления таблицы

--- node_test.js
+++ node_test.js
@@ -35,11 +35,13 @@ http.createServer((request, response) => {
             console.log(result[0]);// приходит либо пустой либо с данными если есть
             if (result[0]) {
                 console.log(` Вы ${result[0].login}  авторизованны`)
-                 check_open(result[0].login)
+               // report ()
+                 //check_open(result[0].login)
             }
             else {
                 console.log('вы не авторизованы');
                 response.end('вы не авторизованы')
+                del()
             }
 
 
@@ -47,7 +49,7 @@ http.createServer((request, response) => {
         }// функция авторизации 
       
 
-        //  as_v_bd('data_kv555') 
+        //  as_v_bd('test111') 
         //  function as_v_bd(new_name_tabl) {
            
         //     let query = "CREATE TABLE `test_baze`.`" + new_name_tabl +"` ( `date` VARCHAR(20) NOT NULL , `electricity_day` VARCHAR(8) NOT NULL , `electricity_night` VARCHAR(8) NOT NULL , `cold_water` VARCHAR(8) NOT NULL , `hot_water` VARCHAR(15) NOT NULL , `id` VARCHAR(11) NOT NULL ) ENGINE = InnoDB"; conn.query(query, (err, result, field) => {
@@ -55,15 +57,20 @@ http.createServer((request, response) => {
         //        console.log(field)
         //        console.log(result)
         //        console.log(err)
-        //     }); // функция  запись в БД новой таблицы с показаниями пользователя при регистрации
+        //     })}; // функция  запись в БД новой таблицы с показаниями пользователя при регистрации
 
     function check_open(log) {
         let query = "SELECT `date`, `electricity_day`, `electricity_night`, `cold_water`, `hot_water`, `id` FROM " + log;
-        console.log(query)
+       // console.log(query)
          conn.query(query, (err, result, field) => {
 
              response.end(JSON.stringify(result[0]))}
           ) };
+          
+          function del (name_table){
+              let query = "DROP TABLE" + name_table
+              conn.query (query)
+          }// удаляет таблицу полностью принимает параметр имя таблицы
 }
 
 ).listen(3001);
